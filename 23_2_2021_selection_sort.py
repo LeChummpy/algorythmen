1eed6dae9bc32258ca450d87a31a5de3226d8df3
LeChummpy <benjamin.jank@gmx.de>
2021-02-24 01:57:57 +0100

    wdnow wokring fine???

--- 23_2_2021_selection_sort.py
+++ 23_2_2021_selection_sort.py
@@ -21,7 +21,7 @@ def selectionsort(liste):
 
 def Nullstellen(a, b, c, d):
 
-    def loeseQuadratischeGleichung(a, b, c):
+    def holersteNullstelle(a, b, c):
         if (a==0):
             if (b==0 and c!=0):
                 return None
@@ -33,28 +33,30 @@ def Nullstellen(a, b, c, d):
             p = b/a
             q = c/a
 
-            Nullstellen=[]
-
             try:
-                x1 = -(p/2) - math.sqrt((p/2)**2 - q)
-                Nullstellen.append(x1)
-            except (ValueError):
-                Nullstellen.append(None)
-
-            try:
-                x2 = -(p/2) + math.sqrt((p/2)**2 - q)
-                Nullstellen.append(x1)
-            except (ValueError):
-                Nullstellen.append(None)
-
-                if Nullstellen[0]==Nullstellen[1]:
-                    return Nullstellen[0]
-
-                else:
-                    return Nullstellen
-
+                x = -(p/2) - math.sqrt((p/2)**2 - q)
+                return x
             except (ValueError):
                 return None
+
+    def holzweiteNullstelle(a, b, c):
+        if (a==0):
+            if (b==0 and c!=0):
+                return None
+
+            elif (b==0 and c==0):
+                return "unendlich"
+
+        else:
+            p = b/a
+            q = c/a
+
+            try:
+                x = -(p/2) + math.sqrt((p/2)**2 - q)
+                return x
+            except (ValueError):
+                return None
+
 
     def MaximaStammfunktion(a, b, c, d, startwert):
         schrittweite = 0.0002/b
@@ -83,21 +85,17 @@ def Nullstellen(a, b, c, d):
             return x
 
     if (a==0): #verhält sich wie quadratische Funktion
-        return loeseQuadratischeGleichung(b, c, d)
+        return (holersteNullstelle(b, c, d), holzweiteNullstelle(b, c, d))
 
     else:
         x1 = finde1Nullstelle(a, b, c, d) #eine Nullstelle mit Gradientenverfahren näherungsweise berechnen, nun mit Hornaverfahern weitere Nullstellen berechnen
 
-        a = a*x1+b
-        b = (a*x1+b)*x1+c
-        c = ((a*x1+b)*x1+c)*x1+d
+        a_ = a*x1+b
+        b_ = (a*x1+b)*x1+c
+        c_ = ((a*x1+b)*x1+c)*x1+d
 
-        print(x1)
-
-        print(a)
-        print(b)
-        print(c)
-        weitere_Nullstellen = loeseQuadratischeGleichung(a, b, c)
-        print(weitere_Nullstellen)
+        x2 = holersteNullstelle(a_, b_, c_)
+        x3 = holzweiteNullstelle(a_, b_, c_)
+        return [x1, x2, x3]
 
 print(Nullstellen(1, -3.7, 1, 1))
